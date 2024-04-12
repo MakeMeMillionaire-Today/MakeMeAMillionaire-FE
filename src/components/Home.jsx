5783ad6c52c98767a533dceea11f1b3557a79b8d
silversurferbeats <gabrielsuarez89@gmail.com>
2024-04-12 16:30:41 -0300

    feat(MainCanvas): integration image selector logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,25 +19,13 @@ const Home = () => {
     reader.readAsDataURL(file);
   };
 
-  const handleRandomMatrixUpdate = () => {
-    rtConnection.emit("/canvas/update", {
-      x: Math.floor(Math.random() * matrix.length),
-      y: Math.floor(Math.random() * matrix.length),
-      content: 1,
-    });
-  };
-
-
   return (
     <div className="home-container">
       <div className="section1">
         chat zone
-        <div>
-          <button onClick={handleRandomMatrixUpdate}>Random Matrix</button>
-        </div>
       </div>
       <div className="section2">
-        <MainCanvas matrix={matrix} />
+        <MainCanvas matrix={matrix} imageURL={imageURL} />
       </div>
       <div className="section3">
         <input type="file" accept="image/*" onChange={handleImageUpload} />
--- src/components/MainCanvas.jsx
+++ src/components/MainCanvas.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { rtConnection } from "../service/socket";
 
 const SIZE = 10;
 
-const MainCanvas = ({ matrix }) => {
-  if (!matrix?.length) return null;
+const MainCanvas = ({ matrix, imageURL }) => {
+  // Handle area:
+  const handleClick = (row, col) => () => {
+    rtConnection.emit("/canvas/update", {
+      x: col,
+      y: row,
+      content: 1,
+    });
+  };
+  // Calculate area:
+  const calculateSelectedArea = () => {
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let maxY = -Infinity;
+
+    matrix.forEach((row, rowIndex) => {
+      row.forEach((selected, colIndex) => {
+        if (selected) {
+          minX = Math.min(minX, colIndex);
+          minY = Math.min(minY, rowIndex);
+          maxX = Math.max(maxX, colIndex);
+          maxY = Math.max(maxY, rowIndex);
+        }
+      });
+    });
+
+    return { minX, minY, maxX, maxY };
+  };
+  const { minX, minY, maxX, maxY } = calculateSelectedArea();
+  const selectedWidth = (maxX - minX + 1) * 10;
+  const selectedHeight = (maxY - minY + 1) * 10;
 
   return (
     <div style={{ position: "relative" }}>
@@ -18,20 +49,39 @@ const MainCanvas = ({ matrix }) => {
               width: SIZE + "px",
               height: SIZE + "px",
               border: "1px solid black",
-              color: colItem === 1 ? 'red' : 'black',
+              color: colItem === 1 ? "red" : "black",
             }}
+            onClick={handleClick(y, x)}
           >
-            <div style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              fontSize: "10px",
-              marginBottom: "5px"
-            }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: "10px",
+                marginBottom: "5px",
+              }}
+            >
               {colItem}
             </div>
           </div>
         ))
+      )}
+      {imageURL && (
+        <div style={{ position: "relative" }}>
+          <img
+            src={imageURL}
+            alt="Selected Image"
+            style={{
+              position: "absolute",
+              top: `${minY * 10}px`,
+              left: `${minX * 10}px`,
+              width: `${selectedWidth}px`,
+              height: `${selectedHeight}px`,
+              zIndex: 1,
+            }}
+          />
+        </div>
       )}
     </div>
   );

ff09a24c0e4196fac156b6a44bc5a6bfb98c142a
silversurferbeats <gabrielsuarez89@gmail.com>
2024-04-15 02:38:01 -0300

    fix(Chat): corrected message chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,9 +4,12 @@ const Chat = () => {
       href="#"
       class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      {/* <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Chat in process
-      </h5>
+      </h5> */}
+      <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+  <span class="font-medium">Hello!</span> chat in progress...
+</div>
       <p class="font-normal text-gray-700 dark:text-gray-400">
         Get ready for the next chat! Let's connect differently.
       </p>

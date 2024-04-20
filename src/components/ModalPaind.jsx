3fbd6600c4d25ae65b026d5c044910d622ac4ceb
silversurferbeats <gabrielsuarez89@gmail.com>
2024-04-20 13:09:20 -0300

    feat(ModalPaint): add clear button to Draws

--- src/components/ModalPaind.jsx
+++ src/components/ModalPaind.jsx
@@ -7,7 +7,7 @@ const ModalPaint = ({ showModal, col, row }) => {
   const canvasRef = useRef(null);
   let isDrawing = false;
 
-  const dibujar = (event) => {
+  const draw = (event) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const x = event.nativeEvent.offsetX;
@@ -15,20 +15,26 @@ const ModalPaint = ({ showModal, col, row }) => {
 
     if (isDrawing) {
       ctx.fillStyle = "black";
-      ctx.fillRect(x, y, 1, 1);
+      ctx.fillRect(x, y, 2, 2);
     }
   };
 
-  const comenzarDibujo = (event) => {
-    isDrawing = true;
-    dibujar(event);
+  const cleanCanvas = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   };
 
-  const detenerDibujo = () => {
+  const startDrawing = (event) => {
+    isDrawing = true;
+    draw(event);
+  };
+
+  const stopDrawing = () => {
     isDrawing = false;
   };
 
-  const exportarImagen = () => {
+  const exportImage = () => {
     const canvas = canvasRef.current;
     const image = canvas.toDataURL("image/png");
     setPaintImg(image);
@@ -38,17 +44,17 @@ const ModalPaint = ({ showModal, col, row }) => {
     showModal(false);
   };
 
-  // handle data to backend: 
+  // handle data to backend:
   useEffect(() => {
     const sendImageData = () => {
       if (paintImg !== "") {
-        console.log('debug --->', row, col, paintImg)
         rtConnection.emit("/canvas/update", {
           y: row,
           x: col,
           image: paintImg,
         });
-        alert("the canvas has been saved!")
+        alert("the canvas has been saved!");
+        handleCloseModal();
       }
     };
     sendImageData();
@@ -57,37 +63,37 @@ const ModalPaint = ({ showModal, col, row }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
       <div className="bg-white p-5 rounded flex flex-col justify-center item-center gap-5">
-        <div>
+        <div className="flex justify-center items-center">
           <canvas
             ref={canvasRef}
-            width={100}
-            height={100}
-            onMouseDown={comenzarDibujo}
-            onMouseMove={dibujar}
-            onMouseUp={detenerDibujo}
+            width={200}
+            height={200}
+            onMouseDown={startDrawing}
+            onMouseMove={draw}
+            onMouseUp={stopDrawing}
             style={{ border: "1px solid black", cursor: "crosshair" }}
           />
         </div>
-        <ul class="flex">
-          <li class="flex-1 mr-2">
+        <ul className="flex">
+          <li className="flex-1 mr-2">
             <button
-              class="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
-              onClick={exportarImagen}
+              className="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
+              onClick={exportImage}
             >
               save
             </button>
           </li>
-          <li class="flex-1 mr-2">
+          <li className="flex-1 mr-2">
             <button
-              class="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
-              // onClick={clear}
+              className="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
+              onClick={cleanCanvas}
             >
               clear
             </button>
           </li>
-          <li class="text-center flex-1">
+          <li className="text-center flex-1">
             <button
-              class="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
+              className="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
               onClick={handleCloseModal}
             >
               close

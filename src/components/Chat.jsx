d57d2e063a07a12ad9d2641747c191d73bd3ebb6
silversurferbeats <gabrielsuarez89@gmail.com>
2024-04-25 19:17:30 -0300

    feat(Chat): add style tailwind in Chat componente

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { rtConnection } from "../service/socket";
-// import { LiMensaje, UlMensajes } from './ui-components';
 
 function Chat() {
   const [isConnected, setIsConnected] = useState(false);
@@ -26,22 +25,30 @@ function Chat() {
   }
 
   return (
-    <div className="App">
-      <h2>{isConnected ? 'ONLINE:' : 'OFFLINE:'}</h2>
-      {/* <UlMensajes> */}
-        {mensajes.map(mensaje => (
-          // <LiMensaje>
-          <div>
-            {mensaje.usuario}: {mensaje.mensaje}
-          </div>
-            // </LiMensaje>
-        ))}
-      {/* </UlMensajes> */}
-      <input
-        type="text"
-        onChange={e => setNuevoMensaje(e.target.value)}
-      />
-      <button onClick={enviarMensaje}>Enviar</button>
+    <div id="chat-container">
+        <div class="bg-white shadow-md rounded-lg max-w-lg w-full">
+            <div class="p-4 border-b bg-fuchsia-900 text-white rounded-t-lg flex justify-between items-center">
+                <p class="text-lg font-semibold">{isConnected ? 'ONLINE:' : 'OFFLINE:'}</p>
+                {/* <button id="close-chat" class="text-gray-300 hover:text-gray-400 focus:outline-none focus:text-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button> */}
+            </div>
+
+            <div id="chatbox" class="p-4 h-64 overflow-y-auto">
+              {mensajes.map(mensaje => (
+                <div class="mb-2 text-right">
+                  <p class="bg-fuchsia-900 text-white rounded-lg py-2 px-4 inline-block">{mensaje.usuario}: {mensaje.mensaje}</p>
+                </div>
+              ))}
+            </div>
+
+            <div class="p-4 border-t flex">
+                <input id="user-input"  type="text" placeholder="Type a message" class="w-full px-3 py-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={e => setNuevoMensaje(e.target.value)} />
+                <button id="send-button" class="bg-fuchsia-900 text-white px-4 py-2 rounded-r-md hover:bg-blue-600 transition duration-300" onClick={enviarMensaje}>Send</button>
+            </div>
+        </div>
     </div>
   );
 }

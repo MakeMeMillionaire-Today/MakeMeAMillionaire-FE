7edd7d986d6c1b7da8defdccfe826177e06cb5ea
silversurferbeats <gabrielsuarez89@gmail.com>
2024-05-03 20:26:33 -0300

    fix(socket): correcter URL backend deploy

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-const rtConnection = io('ws://localhost:5000');
+// const rtConnection = io('ws://localhost:5000');
 // const rtConnection = io('ws://192.168.100.87:5000');
 // const rtConnection = io('ws://192.168.0.1:5000');
+// deploy render:
+const rtConnection = io('ws://makemeamillonarie-be.onrender.com:10000')
 
 export { rtConnection }

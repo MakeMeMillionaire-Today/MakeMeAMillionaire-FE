92b21a8afd58ce8c2c83c5b3e96cd06bdfad04cf
silversurferbeats <gabrielsuarez89@gmail.com>
2024-05-06 17:42:49 -0300

    fix(socket): redeploy test

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
 
-// const rtConnection = io('ws://localhost:5000');
-// const rtConnection = io('ws://192.168.100.87:5000');
-// const rtConnection = io('ws://192.168.0.1:5000');
+// const rtConnection = io('ws://localhost:8080');
+// const rtConnection = io('ws://192.168.100.87:8080');
+// const rtConnection = io('ws://192.168.0.1:8080');
 // deploy home:
-// const rtConnection = io('ws://192.168.56.1:5000')
+// const rtConnection = io('ws://192.168.56.1:8080')
 // deploy render:
-const rtConnection = io('https://makemeamillonarie-be.onrender.com:8080')
+const rtConnection = io('https://makemeamillonarie-be.onrender.com')
 
 export { rtConnection }

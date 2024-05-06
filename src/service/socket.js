e1581165bb235bd955a8bb25f8f44ed287e58bae
silversurferbeats <gabrielsuarez89@gmail.com>
2024-05-06 10:55:28 -0300

    fix(socket): correcte url backend deploy

--- src/service/socket.js
+++ src/service/socket.js
@@ -3,7 +3,9 @@ import { io } from "socket.io-client";
 // const rtConnection = io('ws://localhost:5000');
 // const rtConnection = io('ws://192.168.100.87:5000');
 // const rtConnection = io('ws://192.168.0.1:5000');
+// deploy home:
+// const rtConnection = io('ws://192.168.56.1:5000')
 // deploy render:
-const rtConnection = io('ws://makemeamillonarie-be.onrender.com:10000')
+const rtConnection = io('https://makemeamillonarie-be.onrender.com:10000')
 
 export { rtConnection }

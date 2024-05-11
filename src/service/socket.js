02fdb61069af2fb92cb1f0906630daf6c15e7c68
silversurferbeats <gabrielsuarez89@gmail.com>
2024-05-11 19:49:54 -0300

    fix(socket): new access point by backend

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,11 +1,14 @@
 import { io } from "socket.io-client";
 
+//-> access point by socket.io: 
 // const rtConnection = io('ws://localhost:3000');
 // const rtConnection = io('ws://192.168.100.87:3000');
 // const rtConnection = io('ws://192.168.0.1:3000');
-// deploy home:
+// deploy my red-home:
 // const rtConnection = io('ws://192.168.56.1:3000')
-// deploy render:
-const rtConnection = io('https://makemeamillonarie-be.onrender.com')
+// old deploy render:
+// const rtConnection = io('https://makemeamillonarie-be.onrender.com')
+// new render deploy:
+const rtConnection = io('https://mmm-be.onrender.com')
 
 export { rtConnection }

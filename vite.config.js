115286d4b95658856040bc7a448bcbbff1a1ea46
silversurferbeats <gabrielsuarez89@gmail.com>
2024-05-13 20:48:39 -0300

    fix(vite.config): add new confuguration for enviroment variable

--- vite.config.js
+++ vite.config.js
@@ -8,3 +8,13 @@ export default defineConfig({
   base: process.env.PUBLIC_URL,
   build: { outDir: process.env.BUILD_PATH },
 });
+//-> defineConfig local:
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     "process.env": process.env,
+//     VITE_DOMAIN: process.env.VITE_DOMAIN,
+//     VITE_CLIENT_ID: process.env.VITE_CLIENT_ID,
+//     VITE_MERCADOPAGO_PUBLIC_KEY: process.env.VITE_MERCADOPAGO_PUBLIC_KEY
+//   }
+// })

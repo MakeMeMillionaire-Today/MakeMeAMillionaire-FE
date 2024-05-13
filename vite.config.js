840b8ad4c2789c20db8df253641fafb6bd793379
silversurferbeats <gabrielsuarez89@gmail.com>
2024-05-12 23:18:02 -0300

    fix(viteconfig): corrected configuration

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: './buildConfig/environments',
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-    VITE_DOMAIN: process.env.VITE_DOMAIN,
-    VITE_CLIENT_ID: process.env.VITE_CLIENT_ID,
-    VITE_MERCADOPAGO_PUBLIC_KEY: process.env.VITE_MERCADOPAGO_PUBLIC_KEY
-  }
+  base: process.env.PUBLIC_URL,
+  build: { outDir: process.env.BUILD_PATH },
 });
